76f8fbffb922aa785f28723ecd0f0941c10a6d74
Ali Kaafarani <kaafarani.ali@gmail.com>
2010-03-23 18:09:29 +0100

    Initial commit

---
+++ server/src/no/kvikshaug/reminder/Checker.scala
@@ -0,0 +1,9 @@
+package no.kvikshaug.reminder
+
+import java.util.TimerTask
+
+class Checker extends TimerTask {
+  def run = {
+    println("Whoa, looks like I'm running!")
+  }
+}
---
+++ server/src/no/kvikshaug/reminder/Reminder.scala
@@ -0,0 +1,19 @@
+package no.kvikshaug.reminder
+
+import java.util.{Date, Calendar, Timer}
+
+object Reminder {
+  def main(args: Array[String]) = {
+    val timer = new Timer()
+    val checker = new Checker
+    timer.scheduleAtFixedRate(checker, tomorrowNight, 1000 * 60 * 60 * 24)
+  }
+
+  def tomorrowNight: Date = {
+    val tomorrow = Calendar.getInstance
+    tomorrow.add(Calendar.DATE, 1)
+    tomorrow.set(Calendar.HOUR_OF_DAY, 0)
+    tomorrow.set(Calendar.MINUTE, 10) // ten past, just in case the timing is off when checking our current date
+    return tomorrow.getTime()
+  }
+}
