6fcc80b27b56fe3f71d7671cca11a3459cbdc1a5
Ali Kaafarani <kaafarani.ali@gmail.com>
2010-03-28 21:12:35 +0200

    Have Event fields as constructor params

--- server/src/no/kvikshaug/reminder/data/Event.scala
+++ server/src/no/kvikshaug/reminder/data/Event.scala
@@ -3,9 +3,6 @@ package no.kvikshaug.reminder.data
 import collection.jcl.ArrayList
 import org.joda.time.DateTime
 
-class Event {
-  var name: String
-  var message: String
-  var occurs: String
-  val notificationDates = new ArrayList[DateTime]
+class Event(val name: String, val message: String, val occurs: String,
+            val notificationDates: ArrayList[DateTime]) {
 }
