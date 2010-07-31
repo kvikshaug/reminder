fc040ca8d370264aebdbb0bae14ba0fb74a232da
Ali Kaafarani <kaafarani.ali@gmail.com>
2010-07-31 18:58:42 +0200

    Warn if events.json is non-existant

--- src/main/scala/no/kvikshaug/reminder/Manager.scala
+++ src/main/scala/no/kvikshaug/reminder/Manager.scala
@@ -4,9 +4,10 @@ import com.google.gson.{GsonBuilder}
 import data.{DateTimeSerializer, Event}
 import gson.{ListDeserializer, DateTimeDeserializer}
 import org.joda.time.DateTime
+import java.io.File
 
 object Manager {
-  val jsonFile = "events.json"
+  val jsonFile = new File("events.json")
   val events = List[Event]()
   val gsonbuilder = new GsonBuilder
   gsonbuilder.registerTypeAdapter(classOf[DateTime], new DateTimeSerializer)
@@ -15,6 +16,11 @@ object Manager {
   val gson = gsonbuilder.create
 
   def initialize = {
+    if(!jsonFile.exists) {
+      printf("Error: missing events file, expected path: '%s'.\n", jsonFile.getAbsolutePath)
+      System.exit(-1)
+    }
+
     // load events from disk
     load
     println("some line")
